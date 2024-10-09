154afca6d567118a633d57c6c8c6495051a0cd97
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2024-10-09 04:49:06 +0300

    update on ui

---
+++ app/privacy_policy/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+export default function Component() {
+    return (
+        <div>
+            Privacy Policy
+        </div>
+    )
+}
---
+++ app/service_terms/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+export default function Component() {
+  return (
+    <div>
+        Terms of Service Page
+    </div>
+  )
+}
