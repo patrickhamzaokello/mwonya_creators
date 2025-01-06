dbeb1debe2571dcbb3931eadb6bf9473e284fe7f
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2025-01-06 10:05:01 +0300

    jih

--- app/components/contentUploadForms/TrackUploadForm.tsx
+++ app/components/contentUploadForms/TrackUploadForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
