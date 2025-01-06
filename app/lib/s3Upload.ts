0b16c10b7c3fe90bd1466e14cd1ec4884d931cb2
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2025-01-06 09:30:57 +0300

    fix on s3 upload

--- app/lib/s3Upload.ts
+++ app/lib/s3Upload.ts
@@ -1,5 +1,4 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
-import { PrismaClient } from '@prisma/client'
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
@@ -9,7 +8,6 @@ const s3Client = new S3Client({
   },
 })
 
-const prisma = new PrismaClient()
 
 export async function uploadFileToS3(trackId: number, file: File, fileType: 'track' | 'coverArt') {
   const fileBuffer = await file.arrayBuffer()
@@ -26,37 +24,37 @@ export async function uploadFileToS3(trackId: number, file: File, fileType: 'tra
 
     const awsUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${fileName}`
 
-    // Update track in database
-    if (fileType === 'track') {
-      await prisma.track.update({
-        where: { id: trackId },
-        data: {
-          awsUrl,
-          status: 'UPLOADED',
-        },
-      })
-    } else if (fileType === 'coverArt') {
-      await prisma.track.update({
-        where: { id: trackId },
-        data: {
-          coverArtUrl: awsUrl,
-        },
-      })
-    }
+    // // Update track in database
+    // if (fileType === 'track') {
+    //   await prisma.track.update({
+    //     where: { id: trackId },
+    //     data: {
+    //       awsUrl,
+    //       status: 'UPLOADED',
+    //     },
+    //   })
+    // } else if (fileType === 'coverArt') {
+    //   await prisma.track.update({
+    //     where: { id: trackId },
+    //     data: {
+    //       coverArtUrl: awsUrl,
+    //     },
+    //   })
+    // }
 
     console.log(`${fileType} for track ${trackId} uploaded successfully`)
   } catch (error) {
     console.error(`Error uploading ${fileType} for track ${trackId}:`, error)
 
-    if (fileType === 'track') {
-      // Update track status to FAILED only if the track file upload fails
-      await prisma.track.update({
-        where: { id: trackId },
-        data: {
-          status: 'FAILED',
-        },
-      })
-    }
+    // if (fileType === 'track') {
+    //   // Update track status to FAILED only if the track file upload fails
+    //   await prisma.track.update({
+    //     where: { id: trackId },
+    //     data: {
+    //       status: 'FAILED',
+    //     },
+    //   })
+    // }
   }
 }
 
